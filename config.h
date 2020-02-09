1199799ed2c383420c32c6fa79e997ba6c0d9de5
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2020-02-10 00:29:39 +0300

    use monospace

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FuraCode Nerd Font Mono:size=11";
+static char *font = "monospace:size=11";
 static int borderpx = 1;
 
 /*
